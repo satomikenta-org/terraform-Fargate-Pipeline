6f03de6bb07c81d84945afef71b4b9cba3982e79
里見健太 <satomikennta1213@gmail.com>
2019-06-16 00:59:06 +0900

    initial commit

---
+++ modules/ecs/output.tf
@@ -0,0 +1,28 @@
+output "repository_url" {
+  value = "${aws_ecr_repository.openjobs_app.repository_url}"
+}
+
+output "cluster_name" {
+  value = "${aws_ecs_cluster.cluster.name}"
+}
+
+output "task_def_family" {
+  value = "${aws_ecs_task_definition.web.family}"
+}
+
+
+output "service_name" {
+  value = "${aws_ecs_service.web.name}"
+}
+
+output "alb_dns_name" {
+  value = "${aws_alb.alb_openjobs.dns_name}"
+}
+
+output "alb_zone_id" {
+  value = "${aws_alb.alb_openjobs.zone_id}"
+}
+
+output "security_group_id" {
+  value = "${aws_security_group.ecs_service.id}"
+}
---
+++ production.tf
@@ -0,0 +1,34 @@
+/*====
+Variables used across all modules
+======*/
+locals {
+  production_availability_zones = ["us-east-2a", "us-east-2b"]
+}
+
+provider "aws" {
+  region  = "${var.region}"
+}
+
+
+module "networking" {
+  source               = "./modules/networking"
+  environment          = "production"
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnets_cidr  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets_cidr = ["10.0.10.0/24", "10.0.20.0/24"]
+  region               = "${var.region}"
+  availability_zones   = "${local.production_availability_zones}"
+  key_name             = "practice" # public key for bustion host
+}
+
+
+module "ecs" {
+  source              = "./modules/ecs"
+  environment         = "production"
+  vpc_id              = "${module.networking.vpc_id}"
+  availability_zones  = "${local.production_availability_zones}"
+  repository_name     = "satomi/production"
+  subnets_ids         = ["${module.networking.private_subnets_id}"]
+  public_subnet_ids   = ["${module.networking.public_subnets_id}"]
+  security_groups_ids = ["${module.networking.security_groups_ids}"]
+}
